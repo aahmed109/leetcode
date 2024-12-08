5ea148a0f9a289293f12ad68df26f3448ea7ef78
Akib Ahmed <aahmed109@gmail.com>
2024-12-08 16:50:43 +0800

    Update 0035_Search_Insert_Position.py

--- 0035_Search_Insert_Position.py
+++ 0035_Search_Insert_Position.py
@@ -1,7 +1,7 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         s = 0
-		e = len(nums)-1
+	e = len(nums)-1
         while s <= e:
             m = (s+e)//2
             if nums[m] == target:
