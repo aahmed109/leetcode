378694bb308bba280ef69463b3ccf13e85696868
Akib Ahmed <aahmed109@gmail.com>
2024-12-14 14:34:04 +0800

    Create 1773_Count_Items_Matching_a_Rule.py

---
+++ 1773_Count_Items_Matching_a_Rule.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
+        c=0
+        k=-1
+        if ruleKey == "type":
+            k=0
+        elif ruleKey == "color":
+            k=1
+        else:
+            k=2
+        for i in items:
+            if i[k] == ruleValue:
+                c+=1
+
+        return c
