851d75cf486ede7b9d4853b64e406d6b026cb447
Akib Ahmed <aahmed109@gmail.com>
2025-01-11 16:47:08 +0800

    Update 0414_Third_Maximum_Number.py

--- 0414_Third_Maximum_Number.py
+++ 0414_Third_Maximum_Number.py
@@ -1,9 +1,21 @@
+# O(nlogn) solution
+#class Solution:
+#    def thirdMax(self, nums: List[int]) -> int:
+#        n=list(set(nums))
+#        if len(n) >= 3:
+#            n.sort(reverse=True)
+#            return n[2]
+
+#        else:
+#            return max(n)
+
+# O(n) Solution
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
         n=list(set(nums))
+        
         if len(n) >= 3:
-            n.sort(reverse=True)
-            return n[2]
+            for i in range(2):
+                n.remove(max(n))
 
-        else:
-            return max(n)
+        return max(n)
