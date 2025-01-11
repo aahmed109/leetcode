5342d5df4c20c8f07cc143163ec7f9c9b2c6eeb0
Akib Ahmed <aahmed109@gmail.com>
2025-01-11 17:19:35 +0800

    Update 2085_Count_Common_Words_With_One_Occurrence.py

--- 2085_Count_Common_Words_With_One_Occurrence.py
+++ 2085_Count_Common_Words_With_One_Occurrence.py
@@ -1,36 +1,9 @@
 class Solution:
     def countWords(self, words1: List[str], words2: List[str]) -> int:
-        cc = 0
-        s=0
+        res = 0
 
-        #unc = []
-        res = []
+        for w in words1:
+            if w in words2 and words1.count(w) == 1 and words2.count(w) == 1:
+                res += 1
 
-        while s<len(words1):
-            if words1.count(words1[s])>1:
-                rr = words1[s]
-                res.append(rr)
-                i=words1.count(rr)
-                while i>0:
-                    words1.remove(rr)
-                    i-=1       
-            else: s+=1
-
-        s=0
-        while s<len(words2):
-            if words2.count(words2[s])>1:
-                rr = words2[s]
-                res.append(rr)
-                i=words2.count(rr)
-                while i>0:
-                    words2.remove(rr)
-                    i-=1       
-            else: s+=1
-
-        print(words1)
-        print(words2)
-        for s in words2:
-            if s in words1 and s not in res:
-                cc+=1
-				
-        return cc
+        return res
