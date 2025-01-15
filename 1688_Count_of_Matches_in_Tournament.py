7b034545af4fe855a2a4e25d683986d1632e6ee1
Akib Ahmed <aahmed109@gmail.com>
2025-01-15 19:14:16 +0800

    Create 1688_Count_of_Matches_in_Tournament.py

---
+++ 1688_Count_of_Matches_in_Tournament.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        return n-1
+        # if you wanna see the simulation lol
+        #if n == 1: return 0
+        #if n == 2: return 1
+        
+        #res = 0
+
+        #while n >= 2:
+        #    res += (n%2) + n//2
+        #    n //= 2
+
+        #return res
