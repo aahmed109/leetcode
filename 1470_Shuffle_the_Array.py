0fb891bb5bf114ba7cec484c0820a905a0249f5e
Akib Ahmed <aahmed109@gmail.com>
2025-01-29 18:17:46 +0800

    Create 1470_Shuffle_the_Array.py

---
+++ 1470_Shuffle_the_Array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        res = []
+
+        i = 0
+        j = n
+
+        while i < n:
+            res.append(nums[i])
+            res.append(nums[j])
+            i += 1
+            j += 1
+
+        return res
